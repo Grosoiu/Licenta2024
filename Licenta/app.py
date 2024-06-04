a7e44b5070059954cc75be839a4342f920f3c83e
Andrei-Lucian Grosoiu <andrei.grosoiu@gmail.com>
2024-06-04 22:59:07 +0300

    Schelet final

--- Licenta/app.py
+++ Licenta/app.py
@@ -63,8 +63,13 @@ def display_files():
     return render_template('display_files.html', files=files, action=action)
 
 
-@app.route('/outputs/<filename>')
+@app.route('/uploads/<filename>')
 def uploaded_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+
+@app.route('/outputs/<filename>')
+def output_file(filename):
     return send_from_directory(OUTPUT_FOLDER, filename)
 
 
