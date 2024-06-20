3b95a6bfa920cbdfa314b7ee4cc567894b1d0583
Andrei-Lucian Grosoiu <andrei.grosoiu@gmail.com>
2024-06-21 00:25:44 +0300

    Added convert file functionality

--- Licenta/app.py
+++ Licenta/app.py
@@ -12,7 +12,8 @@ EXECUTABLES = {
     'reverb': 'C:/Users/Grosi/Licenta2024/Licenta/main/reverb_audio.exe',
     'shift': 'C:/Users/Grosi/Licenta2024/Licenta/main/shift_audio.exe',
     'stretch': 'C:/Users/Grosi/Licenta2024/Licenta/main/stretch_audio.exe',
-    'tnnr': 'C:/Users/Grosi/Licenta2024/Licenta/main/executabil.exe'
+    'tnnr': 'C:/Users/Grosi/Licenta2024/Licenta/main/executabil.exe',
+    'convert': 'C:/Users/Grosi/Licenta2024/Licenta/main/convert.exe'
 }
 ALLOWED_EXTENSIONS = {'mp3', 'wav'}
 
@@ -46,22 +47,28 @@ def upload_file(action):
             file.save(filepath)
             # Handle slider values
             command = [EXECUTABLES[action]]
+            output_filename = 'fisier'
             if action == 'stretch':
                 stretch_value = request.form.get('stretch-value', '1')
                 command.append(stretch_value)
             elif action == 'shift':
                 shift_value = request.form.get('shift-value', '0')
                 command.append(shift_value)
+            elif action == 'convert':
+                filetype = request.form.get('filetype', 'wav')
+                command.append(filetype)
+                output_filename += f'.{filetype}'
             subprocess.run(command, shell=True)
-            return redirect(url_for('display_files', action=action))
+            return redirect(url_for('display_files', action=action, output_filename=output_filename))
     return render_template('upload.html', action=action)
 
 
 @app.route('/outputs', methods=['GET'])
 def display_files():
     action = request.args.get('action', 'reverb')
+    output_filename = request.args.get('output_filename', 'fisier.wav')
     files = os.listdir(OUTPUT_FOLDER)
-    return render_template('display_files.html', files=files, action=action)
+    return render_template('display_files.html', files=files, action=action, output_filename=output_filename)
 
 
 @app.route('/uploads/<filename>')
@@ -76,4 +83,3 @@ def output_file(filename):
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
